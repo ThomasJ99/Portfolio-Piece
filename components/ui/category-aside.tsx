0f42d22075d2778e684d525e4fe9e128805381e1
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-04 15:59:15 +0100

    initial version of sidebar, first pass on styling, put page content in a grid where the main content spans 3 columns out of 4

---
+++ components/ui/category-aside.tsx
@@ -0,0 +1,21 @@
+import { getCategories } from "@/data/product";
+import { Category } from "@/types/products-json";
+import CategoryLink from "./category-link";
+
+export default async function CategoryAside() {
+  const categories: Category[] = await getCategories();
+
+  return (
+    <aside className="border-r border-white/10">
+      <h2 className="text-lg font-bold font-oswald opacity-70 mb-3">
+        Categories
+      </h2>
+
+      <ul className="flex flex-col gap-3 ps-5 text-sm font-bold ">
+        {categories.map((c) => (
+          <CategoryLink key={c.id} category={c} />
+        ))}
+      </ul>
+    </aside>
+  );
+}
