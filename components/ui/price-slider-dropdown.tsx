66ba1ca10b0f81a7fbdac88331db4939ef5aadfd
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-05 11:08:04 +0100

    styling for button/dropdown position fixed to be right below button - connecting their borders, added comments to dropdown

--- components/ui/price-slider-dropdown.tsx
+++ components/ui/price-slider-dropdown.tsx
@@ -11,12 +11,16 @@ export default function PriceFilterDropdown({
   min: number;
   max: number;
 }) {
+  // State if the dropdown is open or closed, closed by default
   const [open, setOpen] = useState(false);
-  // Look up useRef to understand why it works
+
+  // Ref that points to the dropdown container element in the DOM
+  // Used to detect clicks outside of the dropdown
   const dropdownRef = useRef<HTMLDivElement>(null);
 
-  // Logic for closing the button if you click outside,
+  // Logic for closing the button if you click outside
   useEffect(() => {
+    // Triggers on mouse click if the dropdown exists and is outside it
     function handleClickOutside(event: MouseEvent) {
       if (
         dropdownRef.current &&
@@ -26,47 +30,51 @@ export default function PriceFilterDropdown({
       }
     }
 
-    // Can turn on and off our function
+    // Can turn on and off our function by listening to all our mouseclicks
+    // Theres definitely some better solution for this...
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   });
 
   return (
     // Knows our div is the one we refer to on useEffect to mimic a dropdown menu
-    <div ref={dropdownRef}>
+    <div className="relative inline-block" ref={dropdownRef}>
       <button
-      onClick={() => setOpen(!open)}
+        onClick={() => setOpen(!open)}
         className={`border-2 px-4 py-3 font-bold hover:bg-gray-900 hover:border-white
-        transition-colors font-oswald uppercase`}
+        transition-colors font-oswald uppercase z-101
+        ${open ? "border-b-0" : ""}`}
       >
-        <span className="flex">
+        <span className="flex gap-3">
           Price{" "}
           {open ? (
             <svg
+              className="mt-1"
               viewBox="0 0 24 24"
-              width="1em"
+              width="0.85em"
               height="1em"
               fill="currentColor"
               focusable="false"
               aria-hidden="true"
             >
-              <path d="M2.64 15.994c0-.192.073-.384.219-.53l7.55-7.55a2.25 2.25 0 0 1 3.181 0l7.551 7.55a.75.75 0 1 1-1.06 1.06l-7.551-7.55a.75.75 0 0 0-1.06 0l-7.55 7.55a.75.75 0 0 1-1.28-.53"></path>
+              <path d="M2.64 15.994c0-.192.073-.384.219-.53l7.55-7.55a2.25 2.25 0 0 1 3.181 0l7.551 7.55a.75.75 0 1 1-1.06 1.06l-7.551-7.55a.75.75 0 0 0-1.06 0l-7.55 7.55a.75.75 0 0 1-1.28-.53" />
             </svg>
           ) : (
             <svg
-              className="w-4 h-4 ms-1.5 -me-0.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
+              className="mt-1"
               viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              width="0.85em"
+              height="1em"
+              fill="none"
+              focusable="false"
+              aria-hidden="true"
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m19 9-7 7-7-7"
               />
             </svg>
@@ -76,15 +84,13 @@ export default function PriceFilterDropdown({
 
       {/* Dropdown */}
       {open && (
-        <div className="absolute left-0 mt-2 w-105 bg-white text-black p-6 shadow-xl border z-50">
+        <div className="absolute left-0 w-105 bg-black border-2 border-white p-6 z-100">
           <PriceSliderDual min={min} max={max} />
 
           <div>
             <button onClick={() => setOpen(false)}>Reset</button>
 
-            <button onClick={() => setOpen(false)}>
-              Save
-            </button>
+            <button onClick={() => setOpen(false)}>Save</button>
           </div>
         </div>
       )}
