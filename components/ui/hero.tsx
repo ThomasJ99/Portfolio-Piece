942ebada8ae57feb8b01ce433efc999a80ada013
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-06 11:30:26 +0100

    hero piece style finalized, adjusted image, text and height of the card

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,48 +1,40 @@
 import Image from "next/image";
+import Link from "next/link";
 
 // *******   FIX:image src, colors, text    ********** //
 
 export default function Hero() {
   return (
-    <header className="">
-      <div className="bg-gray-600">
-        <section
-          aria-label="Company hero section"
-          className="container mx-auto px-50 grid items-center lg:grid-cols-2 gap-2 font-oswald drop-shadow-2xl pt-10"
-        >
-          <div>
-            <h1 className="text-4xl xl:text-5xl font-bold leading-tight text-balance tracking-wide grid col-auto uppercase">
-              New Balance ellipse.
-            </h1>
-            <p className="text-white/90 text-2xl">
-              Fresh Foam X midsole delivers lightweight cushioning for
-              comfortable, everyday running.
-            </p>
-          </div>
+    <header className="bg-black text-white">
+      <section className="container mx-auto grid lg:grid-cols-2 gap-12 items-center py-24">
+        <div>
+          <h1 className="text-5xl font-bold font-oswald uppercase">
+            New Season.
+            <span className="block">New Favorites.</span>
+          </h1>
 
-          <div className="flex">
-            <Image
-              className="shadow-2xl"
-              src="https://cdn.dummyjson.com/product-images/home-decoration/family-tree-photo-frame/1.webp"
-              alt="Banner image of the first volume of Chainsaw Man featuring Denji as the chainsaw devil, slaugthering zombies in stylized fashion"
-              width={300}
-              height={200}
-              preload={true}
-              loading="eager"
-            />
+          <p className="text-lg text-white/80 mt-6 max-w-lg mb-10">
+            Discover trending products, best sellers, and fresh arrivals curated
+            for everyday style and comfort.
+          </p>
 
-            <Image
-              className="shadow-2xl"
-              src="https://cdn.dummyjson.com/product-images/home-decoration/family-tree-photo-frame/1.webp"
-              alt="Banner image of the first volume of Chainsaw Man featuring Denji as the chainsaw devil, slaugthering zombies in stylized fashion"
-              width={300}
-              height={200}
-              preload={true}
-              loading="eager"
-            />
-          </div>
-        </section>
-      </div>
+          <Link
+            className="border-2 border-slate-200 px-6 py-3 uppercase font-bold font-oswald
+            hover:bg-slate-800 transition"
+            href={"/products"}
+          >
+            Shop now
+          </Link>
+        </div>
+
+        <div>
+          {/* TODO: Swap img to Image when I find a good image */}
+          <img
+            src="https://www.shutterstock.com/shutterstock/photos/2558636193/display_1500/stock-photo-woman-shopping-bag-and-travel-for-fashion-in-street-city-and-sunglasses-for-discount-on-commute-2558636193.jpg"
+            alt=""
+          />
+        </div>
+      </section>
     </header>
   );
 }
