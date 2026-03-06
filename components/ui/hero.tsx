2959efed3ec08dc711cb181a8956f0d69fe54cda
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-06 15:08:47 +0100

    improved usage of margin on hero by using space-y instead, tweaked description text and Link element as a inline-block

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -1,33 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
 
-// *******   FIX:image src, colors, text    ********** //
+// https://jsdoc.app/tags-returns
+/**
+ * Use only once, contains h1 and main header of the page
+ */
 
 export default function Hero() {
   return (
     <header className="bg-black text-white">
-      <section className="container mx-auto grid lg:grid-cols-2 gap-12 items-center py-24">
-        <div>
+      <section className="container mx-auto grid lg:grid-cols-2 gap-6 items-center py-24">
+        <div className="space-y-10">
           <h1 className="text-5xl font-bold font-oswald uppercase">
             New Season.
             <span className="block">New Favorites.</span>
           </h1>
 
-          <p className="text-lg text-white/80 mt-6 max-w-lg mb-10">
-            Discover trending products, best sellers, and fresh arrivals curated
-            for everyday style and comfort.
+          <p className="text-lg text-white/80 max-w-lg">
+            Discover fresh arrivals, lighter layers, and everyday essentials
+            made for the new season. Explore trending styles designed for
+            brighter days and warmer weather.
           </p>
 
           <Link
             className="border-2 border-slate-200 px-6 py-3 uppercase font-bold font-oswald
-            hover:bg-slate-800 transition"
+            hover:bg-slate-800 transition-colors inline-block"
             href={"/products"}
           >
             Shop now
           </Link>
         </div>
 
-        <div>
+        <div className="">
           {/* TODO: Swap img to Image when I find a good image */}
           <img
             src="https://www.shutterstock.com/shutterstock/photos/2558636193/display_1500/stock-photo-woman-shopping-bag-and-travel-for-fashion-in-street-city-and-sunglasses-for-discount-on-commute-2558636193.jpg"
