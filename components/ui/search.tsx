95629b3d04da34b6f2f0fc97274ce3d78b1fa909
ThomasJ99 <xmaslink1999@gmail.com>
2026-03-10 11:42:10 +0100

    fixed search bug, it now sends me to products page when i search from any page

--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Form from "next/form";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
@@ -10,20 +10,44 @@ import { SearchIcon } from "lucide-react";
 
 export default function SearchForm() {
   const searchParams = useSearchParams();
+  const router = useRouter();
   const ref = useRef<HTMLFormElement>(null); // HTMLFormElement | null
   const query = searchParams.get("query");
 
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const searchValue = formData.get("query")?.toString() || "";
+
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (searchValue) {
+      params.set("query", searchValue);
+    } else {
+      params.delete("query"); // removes query if empty
+    }
+
+    router.push(`/products?${params.toString()}`);
+  }
+
   return (
-    <Form action="" ref={ref}>
+    <Form action="" onSubmit={handleSubmit} ref={ref}>
       <ButtonGroup>
         <Input
           name="query"
           placeholder="Search..."
           defaultValue={query || ""}
-          className="w-100"
+          // Turnery to turn it into grow instead on wrap
+          className="w-75"
+          required
         />
 
-        <Button type="submit" variant="outline" aria-label="Search">
+        <Button
+          type="submit"
+          variant="outline"
+          aria-label="Search"
+        >
           <SearchIcon />
         </Button>
       </ButtonGroup>
